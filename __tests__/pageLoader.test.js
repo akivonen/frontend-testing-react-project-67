29fc21bd9718ac80544affabd49dabcc9eae1c8c
Artjom Kivonen <kivonen@gmail.com>
2025-09-11 15:36:07 +0300

    feat: add tests for loading images, implement feature in pageLoader

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 import nock from 'nock';
 import os from 'os';
 import { afterAll, beforeEach } from '@jest/globals';
-import pageLoader, { getFilename } from '../src/pageLoader.js';
+import pageLoader, { getNameByPath } from '../src/pageLoader.js';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,7 +11,8 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) =>
   path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+const readFixture = (filename, encoding = 'utf-8') =>
+  fs.readFile(getFixturePath(filename), encoding);
 
 let tmpdir = '';
 
@@ -28,25 +29,38 @@ describe('page-loader', () => {
     nock.enableNetConnect();
   });
 
-  test('Should generate filename from url properly', () => {
-    const filename = getFilename('https://ru.hexlet.io/courses');
+  test('Should generate name from url properly', () => {
+    const filename = getNameByPath('https://ru.hexlet.io/courses', '.html');
     expect(filename).toBe('ru-hexlet-io-courses.html');
+    const foldername = getNameByPath('https://ru.hexlet.io/courses', '_files');
+    expect(foldername).toBe('ru-hexlet-io-courses_files');
   });
 
   test('Should download webpage correctly', async () => {
-    const expectedData = await readFile('index.html');
+    const initHtml = await readFixture('init_index.html');
+    const expectedHtml = await readFixture('expected_index.html');
+    const expectedImg = await readFixture('nodejs.png');
 
     const scope = nock('https://ru.hexlet.io')
       .get('/courses')
-      .reply(200, expectedData);
+      .reply(200, initHtml)
+      .get('/assets/professions/nodejs.png')
+      .reply(200, expectedImg);
 
-    const expectedFilepath = path.join(tmpdir, 'ru-hexlet-io-courses.html');
+    const expectedRootpath = path.join(tmpdir, 'ru-hexlet-io-courses.html');
+    const expectedImgPath = path.join(
+      tmpdir,
+      'ru-hexlet-io-courses_files',
+      'ru-hexlet-io-assets-professions-nodejs.png'
+    );
 
     const response = await pageLoader('https://ru.hexlet.io/courses', tmpdir);
-    const resultData = await fs.readFile(expectedFilepath, 'utf-8');
+    const resultHtml = await fs.readFile(expectedRootpath, 'utf-8');
+    const resultImg = await fs.readFile(expectedImgPath, 'utf-8');
 
     expect(scope.isDone()).toBeTruthy();
-    expect(response.filepath).toBe(expectedFilepath);
-    expect(resultData).toBe(expectedData);
+    expect(response.filepath).toBe(expectedRootpath);
+    expect(resultHtml).toBe(expectedHtml);
+    expect(resultImg).toBe(expectedImg);
   });
 });
--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,14 +1,55 @@
 import path from 'path';
 import fs from 'fs/promises';
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 
-export const getFilename = (url) => {
+export const getNameByPath = (url, extension) => {
   const filename = url
     .toLowerCase()
     .replace(/https?:\/\//, '')
-    .replace('www.', '')
+    .replace(path.extname(url), '')
     .replace(/[^0-9a-z]/gi, '-');
-  return `${filename}.html`;
+  return `${filename}${extension}`;
+};
+
+const loadSources = async (url, html, outputDirpath) => {
+  const $ = cheerio.load(html);
+  const htmlPath = path.join(outputDirpath, getNameByPath(url, '.html'));
+  const assetsFolderpath = path.join(
+    outputDirpath,
+    getNameByPath(url, '_files')
+  );
+  await fs.mkdir(assetsFolderpath);
+
+  const handleAssets = (el) => {
+    const { origin } = new URL(url);
+    const currSrc = $(el).attr('src');
+    const absSrc = currSrc.startsWith('http') ? currSrc : `${origin}${currSrc}`;
+    const extension = path.extname(absSrc);
+    const newFilename = getNameByPath(absSrc, extension);
+
+    const newSrc = path.join(assetsFolderpath, newFilename);
+    const assetsDir = getNameByPath(url, '_files');
+    $(el).attr('src', path.join(assetsDir, newFilename));
+    return { src: absSrc, path: newSrc };
+  };
+
+  const imgList = $('img')
+    .toArray()
+    .map((el) => handleAssets(el));
+
+  await Promise.all([
+    fs.writeFile(htmlPath, $.html()),
+    ...imgList.map(({ src, path }) => {
+      return axios
+        .get(src, { responseType: 'arrayBuffer' })
+        .then(({ data }) => {
+          fs.writeFile(path, data);
+        });
+    }),
+  ]);
+
+  return htmlPath;
 };
 
 export default async function pageLoader(url, outputDirpath = process.cwd()) {
@@ -16,8 +57,7 @@ export default async function pageLoader(url, outputDirpath = process.cwd()) {
     throw new Error('No url provided');
   }
 
-  const filepath = path.join(outputDirpath, getFilename(url));
-  const { data } = await axios.get(url);
-  await fs.writeFile(filepath, data);
+  const { data: html } = await axios.get(url);
+  const filepath = await loadSources(url, html, outputDirpath);
   return { filepath };
 }
