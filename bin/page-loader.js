395ba1da0da1d5d5e19e28a5cd1c88f244f3b82d
Artjom Kivonen <kivonen@gmail.com>
2025-11-02 20:32:56 +0200

    fix(./bin/pageLoader.js): console.log

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -14,7 +14,8 @@ program
   .action(async (url, options) => {
     console.log('Loading website...');
     try {
-      await pageLoader(url, options.output);
+      const { filepath } = await pageLoader(url, options.output);
+      console.log(filepath);
       process.exit(0);
     } catch (e) {
       console.error(e instanceof Error ? e.message : 'Unknown error occurred');
